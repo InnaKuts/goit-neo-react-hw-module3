4bcd431bf3525f5064d9a5247e03efc42abf37cd
Inna Kuts <inna.v.kuts@gmail.com>
2025-05-03 15:48:42 +0200

    save to storage. pre-populate with test data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
@@ -6,13 +6,22 @@ import ContactForm from "./components/ContactForm/ContactForm";
 import { nanoid } from "nanoid";
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem("contacts");
+    return savedContacts
+      ? JSON.parse(savedContacts)
+      : [
+          { id: nanoid(), name: "Rosie Simpson", number: "459-12-56" },
+          { id: nanoid(), name: "Hermione Kline", number: "443-89-12" },
+          { id: nanoid(), name: "Eden Clements", number: "645-17-79" },
+          { id: nanoid(), name: "Annie Copeland", number: "227-91-26" },
+        ];
+  });
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   const handleAddContact = ({ name, number }) => {
     const newContact = {
