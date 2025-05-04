bf466ffe68fd2fd32703b7d3ec708d445a33d957
Inna Kuts <inna.v.kuts@gmail.com>
2025-05-04 18:08:43 +0200

    removed predefined contacts

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,7 @@ import { nanoid } from "nanoid";
 function App() {
   const [contacts, setContacts] = useState(() => {
     const savedContacts = localStorage.getItem("contacts");
-    return savedContacts
-      ? JSON.parse(savedContacts)
-      : [
-          { id: nanoid(), name: "Rosie Simpson", number: "459-12-56" },
-          { id: nanoid(), name: "Hermione Kline", number: "443-89-12" },
-          { id: nanoid(), name: "Eden Clements", number: "645-17-79" },
-          { id: nanoid(), name: "Annie Copeland", number: "227-91-26" },
-        ];
+    return savedContacts ? JSON.parse(savedContacts) : [];
   });
   const [filter, setFilter] = useState("");
 
